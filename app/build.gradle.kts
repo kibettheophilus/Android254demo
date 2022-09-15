1ea0e8941790de7d9cc2ac0e0c3454efc9785dc7
Theophilus Kibet <kibettheophilus@gmail.com>
2022-09-15 18:17:15 +0300

    setup navigation for android flavor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,13 +31,16 @@ android {
     productFlavors {
         create("Android254") {
             applicationIdSuffix = ".android"
+            compileSdk = 33
         }
         val KotlinKenya by creating {
             applicationIdSuffix = ".kotlin"
         }
-
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -48,7 +51,7 @@ android {
     sourceSets {
         getByName("Android254") {
             java {
-                srcDirs("src\\Android254\\java", "src\\Android254\\java")
+                srcDirs("src\\Android254\\java")
             }
         }
     }
@@ -60,6 +63,7 @@ dependencies {
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0-alpha01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
