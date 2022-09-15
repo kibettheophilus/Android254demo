b04da8f8688bec57fce94fb963bf34eabb7dff36
Theophilus Kibet <kibettheophilus@gmail.com>
2022-09-15 17:24:26 +0300

    add resources for kotlin kenya flavor

---
+++ app/src/KotlinKenya/java/com/theo/android254demo/KotlinKenyaMainActivity.kt
@@ -0,0 +1,11 @@
+package com.theo.android254demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinKenyaMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_kenya_main)
+    }
+}
